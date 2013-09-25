0b27441426931014b98898bf4513b60b88fc8595
Gordon Hennesy <gordon@zebrafish.org>
2013-09-25 08:13:04 -0700

    Okay, now we can click the box or button in the upper corner of the window to make the program end.

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 import pygame
-
+import sys
 scr = pygame.display.set_mode((400,440))
 while 1:
+    for event in pygame.event.get():
+         if event.type==pygame.QUIT:
+             sys.exit()
     pygame.display.flip()
