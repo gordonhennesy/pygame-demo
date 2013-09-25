b229e1d9cb5d735d9e4d5546a47a5dcb89abc811
Gordon Hennesy <gordon@zebrafish.org>
2013-09-25 08:25:05 -0700

    Now make a blue square.

--- demo.py
+++ demo.py
@@ -1,6 +1,12 @@
 import pygame
 import sys
 scr = pygame.display.set_mode((400,440))
+red = (255,0,0) # R,G,B, red green blue. The minimum number is 0, max is 255
+green = (0,255,0)
+blue = (0,0,255)
+scr.fill(red)
+square = pygame.Surface((25,25)) # everything you want to display is a Surface. You tell it width, and height
+square.fill(blue) # This says to fill the surface with the color blue
 while 1:
     for event in pygame.event.get():
          if event.type==pygame.QUIT:
