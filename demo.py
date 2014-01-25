0b316876f2c2f8a2136f12d015ca585d86d120d1
Gordon Hennesy <gordon@zebrafish.org>
2014-01-25 01:37:33 -0800

    Added docstring, fixed pylint suggested style

--- demo.py
+++ demo.py
@@ -1,29 +1,42 @@
+""" Demo presents a very minimal introductory program, using pygame to:
+    1. Create and display an object on the screen
+    2. Read in a music file, and loop continuous
+    3. Read in a sound file, and play it in response to a game event
+
+    This will be expanded to include reading in a graphic file,
+    sprite movement, some window methods and others.
+"""
+
 import pygame
 import sys
 pygame.init()
-Fs=22050
-pygame.mixer.init(Fs, -16, 2)
-scr = pygame.display.set_mode((400,440))
-red = (255,0,0) # R,G,B, red green blue. The minimum number is 0, max is 255
-green = (0,255,0)
-blue = (0,0,255)
-scr.fill(red)
-square = pygame.Surface((25,25)) # everything you want to display is a Surface. You tell it width, and height
-square.fill(blue) # This says to fill the surface with the color blue
-background_music = pygame.mixer.music.load('bu-bird-of-an-age.ogg')
+FS = 22050
+pygame.mixer.init(FS, -16, 2)
+SCREEN = pygame.display.set_mode((400, 440))
+RED = (255, 0, 0) # R,G,B, red green blue. The minimum number is 0,
+                  # max is 255
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+SCREEN.fill(RED)
+SQUARE = pygame.Surface((25, 25)) # everything you want to
+                                  # display is a Surface.
+                                  # You tell it width, and height
+SQUARE.fill(BLUE) # This says to fill the surface with the color Bblue
+BACKGROUND_MUSIC = pygame.mixer.music.load('bu-bird-of-an-age.ogg')
 
-loop_forever = -1
-pygame.mixer.music.play(loop_forever)
-explode = pygame.mixer.Sound('cfxrExplosion.wav')
+LOOP_FOREVER = -1
+pygame.mixer.music.play(LOOP_FOREVER)
+EXPLODE = pygame.mixer.Sound('cfxrExplosion.wav')
 while 1:
-    
     for event in pygame.event.get():
-         if event.type==pygame.QUIT:
-             sys.exit()
-         elif event.type==pygame.KEYDOWN:                  
-             if event.key==pygame.K_ESCAPE:
-                 sys.exit()
-             else:
-                 explode.play()
-    scr.blit(square, (200,210)) # blit writes a surface on another surface. The two numbers say where: x and y.
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                sys.exit()
+            else:
+                EXPLODE.play()
+    SCREEN.blit(SQUARE, (200, 210)) # blit writes a surface on another
+                                 # surface. The two numbers say where:
+                                 # x and y.
     pygame.display.flip()
